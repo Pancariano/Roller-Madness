0164be7264117da342cefa0229341492825dc699
Burak Ekemen <bekemen98@gmail.com>
2023-01-13 04:59:43 +0300

    test

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -9,14 +9,14 @@ public class Block : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-           
+
+
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -28,7 +28,7 @@ public class Block : MonoBehaviour
             GetComponent<MeshRenderer>().material.color = Color.red;
             ScoreManager scoreManager = FindObjectOfType<ScoreManager>();
             scoreManager.score--;
-            isCollided =true;
-        }  
+            isCollided = true;
+        }
     }
 }
