005112fc682b493e83d7476730f637a056efcb04
Burak Ekemen <bekemen98@gmail.com>
2023-01-19 04:54:51 +0300

    Scene Loader

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelManager : MonoBehaviour
+{
+    [SerializeField] private GameObject restart;
+    [SerializeField] private GameObject nextLevel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void RestartLevel()
+    {
+        
+    }
+
+    public void NextLevel()
+    {
+        print("Next Level Loaded");
+    }
+}
